22a8cb6b8d079b8c07ffcae7a99fdd1acfcf1696
Federico Donati <realeaquila.929@gmail.com>
2023-07-13 14:30:55 +0000

    TAL - [Alpha 0.3]

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NODE JS' });
+  res.render('index', { title: 'TAL' });
 });
 
 
